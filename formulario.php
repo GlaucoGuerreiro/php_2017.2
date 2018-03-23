a22e21e2686bb272eae290ccc59e9c156a253f30
edimilsoncandido1 <candido.edimilson@gmail.com>
2018-03-22 21:57:41 -0300

    Aula 06 - BD

---
+++ formulario.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="#" method="POST">
+            <fieldset>
+                <div>
+                    <label for="id_nome">Nome:</label>
+                    <input type="text" id="id_nome" name="nome">
+                </div>
+                <div>
+                    <label for="id_matricula">Matricula:</label>
+                    <input type="text" id="id_matricula" name="matricula">
+                </div>
+                <div>
+                    <label for="id_telefone">Telefone:</label>
+                    <input type="tel" id="id_telefone" name="telefone">
+                </div>
+            </fieldset>
+            <button type="submit">Submeter</button>          
+            
+        </form>
+        
+        
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ script_alunos.sql
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  51115152017.2
+ * Created: 22/03/2018
+ */
+    create database escola;
+    
+    use escola;
+
+    create table alunos(
+    Matricula int primary key auto_increment,
+    Nome varchar(100),
+    Telefone varchar(20)
+);
+    
+
+
