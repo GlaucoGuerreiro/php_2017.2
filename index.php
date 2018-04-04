c92b502c3133509c1072e0711fb0c0d87dbdde8d
GlaucoGuerreiro <>
2018-04-03 21:35:35 -0300

    Listas - lógica

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Listas de Exercícios</title>
+    </head>
+    <body>
+        <ul type="circle">
+            <li>Lista 01</li>
+                <ul type="square">
+                    <li><a href="l1q1.php">exercicio 01</a></li>
+                    <li><a href="l1q2.php">exercicio 02</a></li>
+                    <li><a href="l1q3.php">exercicio 03</a></li>
+                    <li><a href="l1q4.php">exercicio 04</a></li>
+                    <li><a href="l1q5.php">exercicio 05</a></li>
+                    
+                </ul>
+            <li>Lista 03</li>
+                <ul type="square">
+                    <li><a href="l3q1.php">exercicio 01</a></li>
+                    <li><a href="l3q2.php">exercicio 02</a></li>
+                    <li><a href="l3q3.php">exercicio 03</a></li>
+                    <li><a href="l1q1.php">exercicio 01</a></li>
+                    <li><a href="l1q1.php">exercicio 01</a></li>
+                    <li><a href="l1q1.php">exercicio 01</a></li>
+                    <li><a href="l1q1.php">exercicio 01</a></li>
+                    <li><a href="l1q1.php">exercicio 01</a></li>
+                </ul>
+        </ul>
+            
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
