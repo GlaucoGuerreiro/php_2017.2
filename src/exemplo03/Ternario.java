7949a53f961093cb5b53f7721a35b9f232d9590f
51115152017.2 <51115152017.2@51DE194244.senacrjedu.local>
2018-05-08 21:47:10 -0300

    Aula03-Java

---
+++ src/exemplo03/Ternario.java
@@ -0,0 +1,12 @@
+package exemplo03;
+
+public class Ternario {
+	public static void main(String[] args) {
+		
+		int var01 = 50;
+		int var02 = 20;
+		
+		System.out.println(var01 > var02 ? "é maior" : "é menor");
+	}
+
+}
